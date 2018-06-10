0290ffdb46ada0b768c60bae239d3c28fca09455
jpalme56 <jpalme56@uwo.ca>
2018-06-10 19:06:45 -0400

    added step aic files

--- cherries.R
+++ cherries.R
@@ -1,5 +1,6 @@
 require(ape)
 require(stringr)
+require(ggplot2)
 setwd("~/PycharmProjects/hiv-evolution-master/9_2_indels/")
 tfolder <- list.files(path="~/PycharmProjects/hiv-evolution-master/8_4_Filtered_Run2", full.names=TRUE)
 vfolder <- list.files(path="~/PycharmProjects/hiv-evolution-master/3RegionSequences/VRegions", full.names=TRUE)
@@ -56,15 +57,7 @@ for (i in 1:length(tfolder)){
   
   
   filtered.indels <- indels[indels$total.length != 0,]
-  # for (y in 1:nrow(indels)){
-  #   value <- indels$total.length[y]
-  #   if (value != 0){
-  #      filtered.indels <- rbind(filtered.indels, data.frame(indels[y,]))
-  #   }
-  # }
-
   
-  temp <- data.frame(stringsAsFactors = F)
   temp2 <- data.frame(stringsAsFactors = F)
   for (x in 1:nrow(filtered.indels)){
     idxA <- match(filtered.indels$tip1.label[x], csv$accno)
@@ -80,12 +73,6 @@ for (i in 1:length(tfolder)){
         bln <- Alength == Blength
         
         A.B <- paste0(Avr,Bvr)
-        temp[x,paste0("VR",(t-1),".A")] <- str_count(A.B, "A")/nchar(A.B)
-        temp[x,paste0("VR",(t-1),".C")] <- str_count(A.B, "C")/nchar(A.B)
-        temp[x,paste0("VR",(t-1),".T")] <- str_count(A.B, "T")/nchar(A.B)
-        temp[x,paste0("VR",(t-1),".G")] <- str_count(A.B, "G")/nchar(A.B)
-        temp[x,paste0("VR",(t-1),".indel")] <- bln
-        
         
         name.bln <- paste0("VR",as.character(t-1),".indel")
         name.nt <- paste0("VR",as.character(t-1),".nt")
@@ -99,14 +86,23 @@ for (i in 1:length(tfolder)){
     }
   }
   
-  write.csv(filtered.indels, filename)
+  #temp2 = output for cherry sequences in csv format (accno1,seq1,accno2,seq2,vregion)
+  #write.csv(temp2,filename)
   
+  #filtered indels = true/false outcomes along with the indel sizes 
+  #write.csv(filtered.indels, filename)
+  
+  for (j in 1:5){
+    len.diff[[paste0(filename,".VR",j,".three")]] <- filtered.indels[,paste0("VR",j,".nt")] <= 3
+    len.diff[[paste0(filename, ".VR",j,".!three")]] <-  filtered.indels[,paste0("VR",j,".nt")] > 3
+  }
+
 }
   
   
 #Used to load the indel.sizes data frame containing 3/6+ indel frequencies
 indel.sizes <- data.frame(stringsAsFactors = FALSE)
-count <- 1
+
 for (z in 1:length(len.diff)){
   subtype <- strsplit(names(len.diff)[[z]], "\\.")[[1]][1]
   vregion <- strsplit(names(len.diff)[[z]], "\\.")[[1]][3]
@@ -130,17 +126,16 @@ for (z in 1:length(len.diff)){
 }
 
 
-
 # for the generation of figures used in poster
 df3 <- data.frame(VRegions=rep(indel.sizes$vregion, indel.sizes$count), Indel_Sizes=rep(indel.sizes$three,indel.sizes$count))
+df4 <- data.frame(Subtype=rep(indel.sizes$subtype, indel.sizes$count), Indel_Sizes=rep(indel.sizes$three,indel.sizes$count))
 table2 <- table(rep(indel.sizes$subtype, indel.sizes$count), rep(indel.sizes$three, indel.sizes$count))
-mosaic(~Variable_Region+Indel_Sizes,data=df3, shade=T, gp_labels=(gpar(fontsize=11)))
 
 par(ps = 29, cex.lab = 1.1, cex.axis = 0.5, cex.sub=0.5, las=0, xpd=T, mar=c(5,4, 0.2,2))
-par(ps = 29, cex.lab = 1.1, cex.axis = 0.5, cex.sub=0.5, las=0, xpd=T, mar=c(5,4, 2,2))
+par(ps = 29, cex.lab = 0.85, cex.axis = 0.5, cex.sub=0.1, las=0, xpd=T, mar=c(5,4, 3,2))
 mosaicplot(~VRegions + Indel_Sizes, data=df3, xlab = "Variable Region", ylab = "Indel Sizes",
            shade=T, main=NULL)
-mosaicplot(~Subtype + Indel_Sizes, data=df3, xlab = "Subtype", ylab = "Indel Sizes",
+mosaicplot(~Subtype + Indel_Sizes, data=df4, xlab = "Subtype", ylab = "Indel Sizes",
            shade=T, main=NULL)
 
 
@@ -170,11 +165,5 @@ mosaicplot(~Subtype + Indel_Sizes, data=df3, xlab = "Subtype", ylab = "Indel Siz
 #     nt.prop[i,paste0('VR',g,'.C.yes')] <- mean(temp[,paste0('VR',g,'.C')][yes])
 #   }
 # }
-# #Used to build the len.diff data frame needed for 3/6+ comparison
-# for (j in 1:5){
-#   len.diff[[paste0(filename,".VR",j,".three")]] <- filtered.indels[,paste0("VR",j,".nt")] <= 3
-#   len.diff[[paste0(filename, ".VR",j,".!three")]] <-  filtered.indels[,paste0("VR",j,".nt")] > 3
-# }
-# if (filename == "B.csv"){
-#   break
-# }
+#Used to build the len.diff data frame needed for 3/6+ comparison
+
