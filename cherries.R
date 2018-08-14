409c3fda921d6cdf3bde7f715291e03fdec8f88f
jpalme56 <jpalme56@uwo.ca>
2018-08-14 12:12:26 -0400

    added figures

--- cherries.R
+++ cherries.R
@@ -18,6 +18,12 @@ for (i in 1:length(tfolder)){
   #for output 
   name <- strsplit(tfolder[i], "/")[[1]][7]
   subtype <- strsplit(name, "\\.")[[1]][1]
+  test <- strsplit(subtype,"_")[[1]]
+  if (length(test) == 2){
+    subtype <- strsplit(subtype,"_")[[1]][2]
+   }else if(subtype == "F1"){
+     subtype <- "F" 
+  }
   filename <- paste0(subtype,"+.csv" )
   
   
@@ -63,7 +69,8 @@ for (i in 1:length(tfolder)){
   filtered.indels <- indels[indels$total.length != 0,]
   
   filtered.indels2 <- data.frame(stringsAsFactors = F)
-  temp3 <- data.frame(stringsAsFactors = F)
+  indel2 <- data.frame(stringsAsFactors = F)
+  nonindel2 <- data.frame(stringsAsFactors = F)
   lens <- c(0,78,120,108,102,33)
   
   count = 0
@@ -72,7 +79,8 @@ for (i in 1:length(tfolder)){
     idxB <- match(filtered.indels$tip2.label[x], csv$accno)
     
     
-    temp2 <- data.frame(stringsAsFactors = F)
+    indel <- data.frame(stringsAsFactors = F)
+    nonindel <- data.frame(stringsAsFactors = F)
     
     #BY VARIABLE LOOPS / COLUMNS 
     for (t in 2:ncol(csv)){
@@ -112,30 +120,41 @@ for (i in 1:length(tfolder)){
       
       #for generating 10_Cherries, only indel-containing sequences
       if (!is.na(bln) && !bln){
-        temp2 <- rbind(temp2, data.frame(accno1=as.character(csv[idxA,1]), seq1=Avr, accno2=as.character(csv[idxB,1]), seq2=Bvr, Vr=(t-1)))
+        indel <- rbind(indel, data.frame(accno1=as.character(csv[idxA,1]), seq1=Avr, accno2=as.character(csv[idxB,1]), seq2=Bvr, Vr=(t-1)))
+      #for generating 9_0_nonindel, containing only nonindel sequences
+      }else if (!is.na(bln) && isTRUE(bln)){
+        nonindel <- rbind(nonindel, data.frame(accno1=as.character(csv[idxA,1]), seq1=Avr, accno2=as.character(csv[idxB,1]), seq2=Bvr, Vr=(t-1)))
       }
       
     } #COLUMNS END
-    temp3 <- rbind(temp3, temp2)
+    indel2 <- rbind(indel2, indel)
+    nonindel2 <- rbind(nonindel2, nonindel)
     filtered.indels2 <- rbind(filtered.indels2, filtered.indels[x,])
     
     
   } #ROWS END 
   print(count)
   
-  setwd("~/PycharmProjects/hiv-evolution-master/10_Cherries2/")
-  #temp3 = output for cherry sequences in csv format (accno1,seq1,accno2,seq2,vregion) FOLDER: 10_Cherries
-  write.csv(temp3,filename)
+  #indel2 = output for cherry sequences in csv format (accno1,seq1,accno2,seq2,vregion) FOLDER: 10_Cherries
+  #only cherries with AT LEAST ONE INDEL are directed here
+  setwd("~/PycharmProjects/hiv-evolution-master/10_Cherries/")
+  write.csv(indel2,filename)
   
   
+  setwd("~/PycharmProjects/hiv-evolution-master/9_0_nonindel/")
+  write.csv(nonindel2,paste0(filename,"+"))
+  
+  #filtered indels = true/false outcomes + indel sizes --- used for MLE analysis  FOLDER: 9_indels
   setwd("~/PycharmProjects/hiv-evolution-master/9_2_indels/")
-  #filtered indels = true/false outcomes along with the indel sizes  FOLDER: 9_indels
-  write.csv(filtered.indels2, filename)
+  write.csv(filtered.indels, filename)
   
   branch.lengths <- rbind(branch.lengths, data.frame(subtype=rep(subtype,nrow(filtered.indels)), length=filtered.indels$total.length))
   for (j in 1:5){
-    len.diff[[paste0(filename,".VR",j,".three")]] <- filtered.indels[which(!is.na(filtered.indels[paste0("VR",j,".indel")])),paste0("VR",j,".nt")] <= 3
-    len.diff[[paste0(filename, ".VR",j,".!three")]] <-  filtered.indels[which(!is.na(filtered.indels[paste0("VR",j,".indel")])),paste0("VR",j,".nt")] > 3
+    name <- paste0("VR",j,".indel")
+    values <- filtered.indels[which(!is.na(filtered.indels[name]) & !filtered.indels[name]),paste0("VR",j,".nt")]
+    len.diff[[paste0(filename,".VR",j,".three")]] <- values == 3
+    len.diff[[paste0(filename, ".VR",j,".six")]] <-  values == 6
+    len.diff[[paste0(filename, ".VR",j,".nine")]] <-  values >= 9
   }
 
 }
@@ -151,35 +170,67 @@ for (z in 1:length(len.diff)){
   
   indel.sizes[z,"subtype"] <- subtype #vregion
   indel.sizes[z,"count"] <- sum(len.diff[[z]])
-  indel.sizes[z,"vregion"] <- vregion
+  indel.sizes[z,"vregion"] <- paste0("V",vregion)
   if (size == "three"){
-    indel.sizes[z,"three"] <- "<= 3 nt"
+    indel.sizes[z,"size"] <- "3"
+  }else if (size == "six"){
+    indel.sizes[z,"size"] <- "6"
   }else{
-    indel.sizes[z,"three"] <- "> 3 nt"
+    indel.sizes[z,"size"] <- "9+"
   }
   
 }
 
 
 # MOSAIC PLOTS - Figure 3
-df3 <- data.frame(VRegions=rep(indel.sizes$vregion, indel.sizes$count), Indel_Sizes=rep(indel.sizes$three,indel.sizes$count))
-df4 <- data.frame(Subtype=rep(indel.sizes$subtype, indel.sizes$count), Indel_Sizes=rep(indel.sizes$three,indel.sizes$count))
-table2 <- table(rep(indel.sizes$subtype, indel.sizes$count), rep(indel.sizes$three, indel.sizes$count))
+df3 <- data.frame(variable.loop=rep(indel.sizes$vregion, indel.sizes$count), indel.size=rep(indel.sizes$size,indel.sizes$count),stringsAsFactors = F)
+df4 <- data.frame(subtype=rep(indel.sizes$subtype, indel.sizes$count), indel.size=rep(indel.sizes$size,indel.sizes$count),stringsAsFactors = F)
+
+df3$indel.size <- factor(df3$indel.size,levels=c("9+","6","3"))
+df3 <- df3[order(df3$indel.size),]
+
+df4$indel.size <- factor(df4$indel.size,levels=c("9+","6","3"))
+df4 <- df4[order(df4$indel.size),]
 
 require(vcd)
-par(ps = 50, cex.lab = 0.7, cex.axis = 0.5, cex.sub=0.5, las=0, xpd=T, mar=c(5,4, 2,2), mfrow=c(2,2))
-mosaic(~VRegions + Indel_Sizes, data=df3,
-       shade=T, main=NULL, direction="v",spacing=spacing_equal(sp = unit(0, "lines")),
-       residuals_type=NULL,legend=T)
+library(gridGraphics)
+library(gridExtra)
+#par(ps = 50, cex.lab = 0.7, cex.axis = 0.5, cex.sub=0.5, las=0, xpd=T, mar=c(5,4, 2,2), mfrow=c(2,2))
 
 
+mosaic(~variable.loop + indel.size, data=df3,
+       shade=T, main=NULL, direction="v",
+       spacing=spacing_equal(sp = unit(0.7, "lines")),
+       residuals_type="Pearson",legend=T,
+       margins=c(2,2,4,2),
+       labeling_args = list(tl_labels = c(F,T), 
+                            tl_varnames=c(F,T),
+                            gp_labels=gpar(fontsize=15),
+                            gp_varnames=gpar(fontsize=19),
+                            set_varnames = c(variable.loop="Variable Loop", 
+                                             indel.size="Indel Length (nt)"),
+                            offset_labels=c(0,0,0,0),rot_labels=c(0,0,0,90), just_labels=c("center","center","center","center")),
+       set_labels=list(Variable.Loop=c("V1","V2","V3","V4","V5")))
+a <- grid.grab()
 
-          
-par(ps = 27, cex.lab = 0.7, cex.axis = 0.5, cex.sub=0.1, las=0, xpd=T, mar=c(5,4, 2,2), xaxt='n')
-mosaic(~Subtype + Indel_Sizes, data=df4, xlab = "Subtype", ylab = "Indel Sizes",
-       shade=T, main=NULL, direction="v",spacing=spacing_equal(sp = unit(0, "lines")),
-       residuals_type=NULL,legend=T, set_varnames = c(Sex="Gender", Survived="survival"))
-
+#par(ps = 27, cex.lab = 0.7, cex.axis = 0.5, cex.sub=0.1, las=0, xpd=T, mar=c(5,4, 2,2), xaxt='n')
+mosaic(~subtype + indel.size, data=df4,
+       shade=T, main=NULL, direction="v",
+       spacing=spacing_equal(sp = unit(0.7, "lines")),
+       residuals_type="Pearson",legend=F, 
+       margins=c(1,4,4,4),
+       labeling_args = list(tl_labels = c(F,T), 
+                            tl_varnames=c(F,T), 
+                            gp_labels=gpar(fontsize=15),
+                            gp_varnames=gpar(fontsize=19),
+                            set_varnames = c(subtype="Subtype", 
+                                             indel.size="Indel Length (nt)"),
+                            offset_labels=c(0,0,0,0),rot_labels=c(0,0,35,90), 
+                            just_labels=c("center","center","center","center"))
+)
+b <- grid.grab()
+grid.newpage()
+grid.arrange(b,a,ncol=2)
 
 #PHYLOGENETIC PLOT - Figure 1
 tre <- read.tree(tfolder[7])
