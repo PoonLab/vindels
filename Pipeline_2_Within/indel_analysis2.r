3dd5751129308d8b1dee00f49a9f9ef8d6b1bfa7
John Palmer <jpalme56@uwo.ca>
2019-04-02 10:27:10 -0400

    updated indel analysis

--- Pipeline_2_Within/indel_analysis2.r
+++ Pipeline_2_Within/indel_analysis2.r
@@ -30,7 +30,7 @@ extractInfo <- function(input){
 
 reconfolder <- Sys.glob("/home/jpalmer/PycharmProjects/hiv-withinhost/9Indels/*")
 
-data.df <- data.frame(name=character(), date=numeric(), count=integer())
+data.df <- data.frame()
 #for (file in reconfolder){
 file <- "~/PycharmProjects/hiv-withinhost/9Indels/30631-a_recon.csv"
 recon <- read.csv(file, sep="\t")
@@ -39,13 +39,13 @@ insertion <- data.frame(recon[,c(1:3)])
 insertion$count1 <- sapply(insertion$Ins1, csvcount) 
 insertion$count2 <- sapply(insertion$Ins2, csvcount)
   
+insertion <- cbind( as.data.frame(t(sapply(insertion$AccNo, extractInfo))), insertion)
+insertion$AccNo <- NULL
+
 insertion$name1 <- unlist(insertion$name1)
 insertion$name2 <- unlist(insertion$name2)
 insertion$date1 <- unlist(insertion$date1)
 insertion$date2 <- unlist(insertion$date2)
-  
-insertion <- cbind( as.data.frame(t(sapply(insertion$AccNo, extractInfo))), insertion)
-insertion$AccNo <- NULL
   
 data.df <- rbind(data.df, data.frame(name=insertion$name1, date=insertion$date1, ins=insertion$Ins1, count=insertion$count1))
 data.df <- rbind(data.df, data.frame(name=insertion$name2, date=insertion$date2, ins=insertion$Ins2, count=insertion$count2))
