38590c37388f4189268c1b8a00c49f96d3570097
John Palmer <jpalme56@uwo.ca>
2019-08-20 12:15:26 -0400

    deleted Phylo_indels folder

--- Phylo_Indels/fasttree.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for filename in /home/jpalme56/PycharmProjects/hiv-evolution-master/5_1_final/*.fasta; do
-	fasttree -nt "$filename" > "${filename}.tree"
-done
--- Phylo_Indels/msa.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-for filename in /home/jpalme56/PycharmProjects/hiv-evolution-master/4_1_Edited/*.fasta; do
-	mafft --auto "$filename" > "${filename}.msa"
-done
