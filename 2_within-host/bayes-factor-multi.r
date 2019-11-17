7aa3a15c57e43b76362eb351aac0b1e63c992a1f
John Palmer <jpalme56@uwo.ca>
2019-11-16 20:00:12 -0500

    created bayes-factor-multi.r

---
+++ 2_within-host/bayes-factor-multi.r
@@ -0,0 +1,66 @@
+args <- commandArgs(trailingOnly=T)
+
+# if (length(args)!= 2){
+# 
+# 	print("USAGE: Rscript bayes-factor.R [input dir 1] [input dir 2] ...")
+# 	quit()
+# }
+for (i in 1:length(args)){
+	if (!endsWith(args[i],"/")){
+		args[i] <- paste0(args[i],"/")
+	}
+}
+commonfiles <- c()
+for (i in 1:length(args)){
+  logfiles <- c()
+  logfiles <- Sys.glob(paste0(args[i],"*.log"))
+  logfiles <- unname(sapply(logfiles, basename))
+  #print(logfiles)
+  if (i == 1){
+    commonfiles <- logfiles
+  }else{
+    toRemove <- sapply(commonfiles, function(x){if (!(x %in% logfiles)) x })
+    toRemove <- -c(which(toRemove %in% commonfiles))
+    if (length(toRemove) > 0){
+      commonfiles <- commonfiles[toRemove]
+    }
+  }
+}
+print(commonfiles)
+bayes <- data.frame(filename=commonfiles, stringsAsFactors = F)
+runs <- c()
+for (j in 1:length(args)){
+  logfiles <- paste0(args[j],commonfiles)
+  means <- c()
+  
+
+  # ITERATOR: DIRECTORY NUMBER 1
+  for (k in 1:length(logfiles)){
+    filename <- basename(logfiles[k])
+
+  
+    # DIRECTORY NUMBER 1 PROCESSING
+    data <- read.table(logfiles[k], sep="\t", skip=4, head=T)
+    loglen <- nrow(data) -1
+    #print(loglen)
+    
+    # calculates the start and end interval of MCMC steps AFTER the burn in (assuming last 90%)
+    interval <- c(loglen*0.1+1,loglen+1)
+    #print(interval)
+
+    llh <- mean(data$likelihood[interval[1]:interval[2]])
+    #print(llh)
+    means[k] <- llh
+  }
+  bayes[,j+1] <- means
+  runs[j] <- args[j]
+}
+
+bayes$best <- apply(bayes[,1:j+1], 1, function(x) which(x == max(x)))
+colnames(bayes) <- c("filename", runs, "best")
+write.csv(bayes, "~/PycharmProjects/hiv-withinhost/6_hm/bayes-comparison.csv")
+for (x in 1:j){
+  print(paste0("DIR",x," Count: ", sum(bayes$best == x)))
+}
+
+#print(paste0("DIR2 Count: ", sum(bayes$better == 2)))
--- 2_within-host/bayes-factor.r
+++ 2_within-host/bayes-factor.r
@@ -1,78 +1,66 @@
 args <- commandArgs(trailingOnly=T)
 
 if (length(args)!= 2){
-
-	print("USAGE: Rscript bayes-factor.R [input dir 1] [input dir 2] ...")
-	quit()
+  
+  print("USAGE: Rscript bayes-factor.R [input dir 1] [input dir 2]")
+  quit()
 }
 for (i in 1:length(args)){
-	if (!endsWith(args[i],"/")){
-		args[i] <- paste0(args[i],"/")
-	}
-}
-commonfiles <- c()
-for (dtry in args){
-  logfiles <- c()
-  logfiles <- Sys.glob(paste0(dtry,"*.log"))
-  
+  if (!endsWith(args[i],"/")){
+    args[i] <- paste0(args[i],"/")
+  }
 }
 
-for (dtry in args){
-  logfiles <- Sys.glob(paste0(dtry,"*.log"))
-  smeans <- c()
-  rmeans <- c()
-  files <- c()
+logfiles <- Sys.glob(paste0(args[1],"*.log"))
+smeans <- c()
+rmeans <- c()
+files <- c()
+# ITERATOR: DIRECTORY NUMBER 1 
+for (fullpath in logfiles){
+  filename <- basename(fullpath)
   
-  # ITERATOR: DIRECTORY NUMBER 1 
-  for (fullpath in logfiles){
-    filename <- basename(fullpath)
-    
-    # DIRECTORY NUMBER 2
-    strpath <- paste0(args[2], filename)
+  # DIRECTORY NUMBER 2
+  strpath <- paste0(args[2], filename)
+  #print(filename)
+  if (file.exists(strpath)){
     #print(filename)
-    if (file.exists(strpath)){
-      #print(filename)
-      #print(strpath)
-      
-      # DIRECTORY NUMBER 1 PROCESSING 
-      relaxed <- read.table(fullpath, sep="\t", skip=3, head=T)
-      loglen <- nrow(relaxed) -1
-      #print(loglen)
-      
-      # calculates the start and end interval of MCMC steps AFTER the burn in (assuming last 90%)
-      interval <- c(loglen*0.1+1,loglen+1)
-      #print(interval)
-      
-      rhm <- mean(relaxed$likelihood[interval[1]:interval[2]])
-      
-      # DIRECTORY NUMBER 2 PROCESSING
-      strict <- read.table(strpath, sep="\t", skip=3, head=T)
-      loglen <- nrow(strict) -1
-      #print(loglen)
-      if (loglen != 10000){
-        next
-      }
-      # calculates the start and end interval of MCMC steps AFTER the burn in (assuming last 90%)
-      interval <- c(loglen*0.1+1,loglen+1)
-      #print(interval)
-      
-      shm <- mean(strict$likelihood[interval[1]:interval[2]])
-      #print(strict$likelihood) 
-      
-      #print(rhm)
-      #print(shm)
-      
-      smeans <- c(smeans, shm)
-      rmeans <- c(rmeans, rhm)
-      files <- c(files, filename)
-    }
+    #print(strpath)
     
+    # DIRECTORY NUMBER 1 PROCESSING 
+    relaxed <- read.table(fullpath, sep="\t", skip=3, head=T)
+    loglen <- nrow(relaxed) -1
+    #print(loglen)
+    
+    # calculates the start and end interval of MCMC steps AFTER the burn in (assuming last 90%)
+    interval <- c(loglen*0.1+1,loglen+1)
+    #print(interval)
+    
+    rhm <- mean(relaxed$likelihood[interval[1]:interval[2]])
+    
+    # DIRECTORY NUMBER 2 PROCESSING
+    strict <- read.table(strpath, sep="\t", skip=3, head=T)
+    loglen <- nrow(strict) -1
+    #print(loglen)
+    if (loglen != 10000){
+      next
+    }
+    # calculates the start and end interval of MCMC steps AFTER the burn in (assuming last 90%)
+    interval <- c(loglen*0.1+1,loglen+1)
+    #print(interval)
+    
+    shm <- mean(strict$likelihood[interval[1]:interval[2]])
+    #print(strict$likelihood) 
+    
+    #print(rhm)
+    #print(shm)
+    
+    smeans <- c(smeans, shm)
+    rmeans <- c(rmeans, rhm)
+    files <- c(files, filename)
   }
   
 }
 
-bayes <- data.frame(filename=files, DIR1=rmeans, DIR2=smeans, diff=rmeans-smeans)
+bayes <- data.frame(filename=files, DIR1=rmeans, DIR2=smeans)
 bayes$better <- mapply(function(x,y){if (x > y){1} else 2}, bayes$DIR1, bayes$DIR2)
 print(bayes)
-print(paste0("DIR1 Count: ", sum(bayes$better == 1)))
-print(paste0("DIR2 Count: ", sum(bayes$better == 2)))
