7909a762a199d633de93b8e21807629a5e553c01
jpalmer37 <jpalme56@uwo.ca>
2020-03-19 10:51:19 -0400

    updated m2.r

--- 2_within-host/modeling2.r
+++ 2_within-host/modeling2.r
@@ -532,7 +532,7 @@ runMCMC <- function(startvalue, iterations, slip.list){
       accept <- F
     }
     print(paste("STATE",i,":", chain[i,1], chain[i,2], chain[i,3], sep=" "))
-    write(paste(c(chain[1,], as.numeric(s.change), as.numeric(accept)),collapse=",") , file=logfile, append=T)
+    write(paste(c(chain[i,], as.numeric(s.change), as.numeric(accept)),collapse=",") , file=logfile, append=T)
   }
   return(list(chain=chain, slip=slip_current))
   close(logfile)
@@ -543,36 +543,6 @@ startvalue <- c(0.001, 0.8, 0.001)
 chain <- runMCMC(startvalue, 50000, slip.list)
 
 
-# sets the burnin size, removes all rows from the chain that are associated with the burnin 
-burnin <- 0.1*(nrow(chain)-1)
-acceptance <- 1 - mean(duplicated(chain[-(1:burnin),]))
-print(paste0("Acceptance: ", acceptance))
-
-
-med1 <- round(median(chain[-(1:burnin),1]),4)
-med2 <- round(median(chain[-(1:burnin),2]),4)
-med3 <- round(median(chain[-(1:burnin),3]),4)
-par(mfrow=c(1,3), mar=c(5,5,4,1))
-# PLOTTING 
-hist(chain[-(1:burnin),1],nclass=30, main="Posterior of Enter", xlab="Prob(Enter)",ylab="Frequency",col="lightskyblue")
-abline(v = med1, col='red',lwd=2)
-text(0.000168, 70000, paste0("Median = ", med1))
-text(0.000168, 60000, paste0("Acceptance = ", as.character(acceptance)))
-hist(chain[-(1:burnin),2],nclass=30, main="Posterior of Stay", xlab="Prob(Stay)", ylab="Frequency",col="lightskyblue")
-text(0.905, 70000, paste0("Median = ", med2))
-abline(v = med2, col='red',lwd=2)
-hist(chain[-(1:burnin),3],nclass=30, main="Posterior of Rate", xlab="Rate", ylab="Frequency",col="lightskyblue")
-#text(0.00017, 400, paste0("Acceptance = ",as.character(acceptance)))
-dev.off()
-
-len <- length(chain[,1])
-thinned <- seq(burnin, len, 100)
-
-par(mfrow=c(1,2), mar=c(5,5,4,1))
-plot(chain[thinned,1], type = "l", xlab="MCMC Steps" , ylab="Prob(Enter)",main = "Chain values of Enter")
-abline(h = med1, col="red")
-plot(chain[thinned,2], type = "l", xlab="MCMC Steps" , ylab="Prob(Stay)",main = "Chain values of Stay")
-abline(h = med2, col="red")
 
 # get slip locations 
 
