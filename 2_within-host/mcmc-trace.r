9bb7aa8ed2c42707c11b9156371deff52286ab14
John Palmer <jpalme56@uwo.ca>
2020-04-24 14:21:55 -0400

    updated mcmc

--- 2_within-host/mcmc-trace.r
+++ 2_within-host/mcmc-trace.r
@@ -1,5 +1,8 @@
 # ----- SLIPPAGE MODEL -----
 csv <- read.csv("~/PycharmProjects/hiv-withinhost/15_modeling/slip-11-nofix.csv", stringsAsFactors = F, comment.char="#")#, skip=1, header=F)
+csv2 <- read.csv("~/PycharmProjects/hiv-withinhost/15_modeling/slip-10-fix3.csv", stringsAsFactors = F, comment.char="#")
+
+csv 
 
 # csv <- as.data.frame(sapply(1:ncol(csv), function(x){
 #   sapply(1:nrow(csv), function(y){
@@ -11,7 +14,7 @@ tru <- c(0.00016, 0.75, 0.00001, 0.12)
 #colnames(csv) <- c('p.enter', 'p.stay', 'rate', 'fix','fix-sd', 'slip.changed', 'accept', 'time')
 # colnames(csv) <- c('p.enter', 'slope','int', 'rate', 'slip.changed', 'accept', 'time')
 #png(file="~/vindels/Figures/within-host/finalized/slippage-trace2.png", width=600, height=800)
-par(mar=c(2.5,5,3,1), mfrow=c(4,1))
+par(mar=c(2.5,5,3,1), mfrow=c(2,1))
 plot(csv[-(1:burnin)
          ,1], type = "l", xlab="MCMC Steps (x10)" , ylab="Prob(Enter)",
      main = "Chain values of Enter", cex.axis=1.3, cex.lab=1.4, cex.main=1.7)#, ylim=c(0.00015, 0.00025))
@@ -22,7 +25,7 @@ plot(csv[-(1:burnin)
 abline(h=tru[2],col='red',lwd=2)
 plot(csv[#-(1:burnin)
          ,3], type = "l", xlab="MCMC Steps (x10)" , ylab="Rate",
-     main = "Chain values of Rate",  cex.axis=1.3, cex.lab=1.4, cex.main=1.7, ylim=c(0.00001,0.000015))
+     main = "Chain values of Rate",  cex.axis=1.3, cex.lab=1.4, cex.main=1.7)# ylim=c(0.00001,0.000015))
 abline(h=tru[3],col='red',lwd=2)
 plot(csv[-(1:burnin)
          ,4], type = "l", xlab="MCMC Steps (x10)" , ylab="Fix",
