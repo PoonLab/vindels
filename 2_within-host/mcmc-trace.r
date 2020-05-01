d17152df4c4a1dad353f4575f8da21c2ddb8feea
jpalmer37 <jpalme56@uwo.ca>
2020-05-01 13:57:41 -0400

    updated trace, added posterior

--- 2_within-host/mcmc-trace.r
+++ 2_within-host/mcmc-trace.r
@@ -1,5 +1,5 @@
 # ----- SLIPPAGE MODEL -----
-csv <- read.csv("~/PycharmProjects/hiv-withinhost/15_modeling/slip-11-nofix.csv", stringsAsFactors = F, comment.char="#")#, skip=1, header=F)
+csv <- read.csv("~/PycharmProjects/hiv-withinhost/15_lmodeling/slip-9-fix2.csv", stringsAsFactors = F, comment.char="#")#, skip=1, header=F)
 csv2 <- read.csv("~/PycharmProjects/hiv-withinhost/15_modeling/slip-10-fix3.csv", stringsAsFactors = F, comment.char="#")
 
 csv 
@@ -14,7 +14,7 @@ tru <- c(0.00016, 0.75, 0.00001, 0.12)
 #colnames(csv) <- c('p.enter', 'p.stay', 'rate', 'fix','fix-sd', 'slip.changed', 'accept', 'time')
 # colnames(csv) <- c('p.enter', 'slope','int', 'rate', 'slip.changed', 'accept', 'time')
 #png(file="~/vindels/Figures/within-host/finalized/slippage-trace2.png", width=600, height=800)
-par(mar=c(2.5,5,3,1), mfrow=c(3,1))
+par(mar=c(2.5,5,3,1), mfrow=c(2,2))
 plot(csv[-(1:burnin)
          ,1], type = "l", xlab="MCMC Steps (x10)" , ylab="P(Enter)",
      main = "Chain values of Enter", cex.axis=1.3, cex.lab=1.4, cex.main=1.7)#, ylim=c(0.00015, 0.00025))
@@ -23,9 +23,9 @@ plot(csv[-(1:burnin)
          ,2], type = "l", xlab="MCMC Steps (x10)" , ylab="P(Stay)",
      main = "Chain values of Stay",  cex.axis=1.3, cex.lab=1.4, cex.main=1.7)#, ylim=c(0.62, 0.77))
 abline(h=tru[2],col='red',lwd=2)
-plot(csv[#-(1:burnin)
+plot(csv[-(1:burnin)
          ,3], type = "l", xlab="MCMC Steps (x10)" , ylab="Rate",
-     main = "Chain values of Rate",  cex.axis=1.3, cex.lab=1.4, cex.main=1.7)# ylim=c(0.00001,0.000015))
+     main = "Chain values of Rate",  cex.axis=1.3, cex.lab=1.4, cex.main=1.7)#, ylim=c(0.0001,0.00015))
 abline(h=tru[3],col='red',lwd=2)
 plot(csv[-(1:burnin)
          ,4], type = "l", xlab="MCMC Steps (x10)" , ylab="Fix",
@@ -35,7 +35,25 @@ plot(csv[-(1:burnin)
          ,'fix-sd'], type = "l", xlab="MCMC Steps (x10)" , ylab="Fix - SD",
      main = "Chain values of Fix SD",  cex.axis=1.3, cex.lab=1.4, cex.main=1.7)#, ylim=c(0.0001,0.00015))
 
-
+par(mar=c(6,5.5,6,1),mfrow=c(1,3))
+hist(csv[-(1:burnin),1], 
+     col="dodgerblue", 
+     breaks=15, 
+     main="Posterior of 'Enter'", 
+     xlab="P(Enter)", cex.axis=1.3, cex.lab=1.5, cex.main=1.8)
+abline(v=tru[1],col='red',lwd=2)
+hist(csv[-(1:burnin),2], 
+     col="dodgerblue", 
+     breaks=15, 
+     main="Posterior of 'Stay'", 
+     xlab="P(Stay)", cex.axis=1.3, cex.lab=1.5, cex.main=1.8)
+abline(v=tru[2],col='red',lwd=2)
+hist(csv[-(1:burnin),3], 
+     col="dodgerblue", 
+     breaks=15, 
+     main="Posterior of 'Rate'", 
+     xlab="Rate", cex.axis=1.3, cex.lab=1.5, cex.main=1.8)
+abline(v=tru[3],col='red',lwd=2)
 med1 <- median(csv$p.enter)
 med2 <- median(csv$p.stay)
 med3 <- median(csv$rate)
