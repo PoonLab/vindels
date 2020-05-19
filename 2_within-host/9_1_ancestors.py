75e122462223bba0a6ff3ab20a7fd404d8df307a
jpalmer37 <jpalme56@uwo.ca>
2020-05-19 15:02:00 -0400

    updated 9_1

--- 2_within-host/9_1_ancestors.py
+++ 2_within-host/9_1_ancestors.py
@@ -12,7 +12,6 @@ from ete2 import *
 from seqUtils import * 
 from glob import * 
 from ancestors import *
-t = Tree("(((((((C.-.-.QJ.16.-.-_198:13.49219,((C.-.-.QJ.07.-.-_198:1.45601,C.-.-.QJ.13.-.-_198:1.45601):14.30421,C.-.-.QJ.02.-.-_198:15.76022):270.71697):20.55638,C.-.-.QJ.15.-.-_198:307.03358):30.77864,C.-.-.QJ.08.-.-_198:337.81221):22.21124,((C.-.-.QJ.10.-.-_344:120.04968,(C.-.-.QJ.11.-.-_344:65.53720,C.-.-.QJ.05R.-.-_377:98.53720):54.51249):29.65029,(((((C.-.-.QJ.07R.-.-_471:43.50810,C.-.-.QJ.14R.-.-_471:43.50810):10.44232,C.-.-.QJ.12R.-.-_471:53.95041):12.64329,C.-.-.QJ.16R.-.-_471:66.59370):54.59621,(C.-.-.QJ.01R.-.-_471:31.51815,C.-.-.QJ.08R.-.-_471:31.51815):89.67176):128.24699,(C.-.-.QJ.04.-.-_344:70.61993,C.-.-.QJ.13.-.-_344:70.61993):51.81697):27.26308):83.33848):434.35455,((C.-.-.QJ.06.-.-_198:91.95698,C.-.-.QJ.14.-.-_198:91.95698):329.99340,((C.-.-.QJ.11.-.-_071:9.35085,C.-.-.QJ.03.-.-_071:9.35085):284.19195,(((C.-.-.QJ.03.-.-_198:408.18850,((C.-.-.QJ.18R.-.-_008:32.94331,C.-.-.QJ.07.-.-_071:95.94331):131.80356,((C.-.-.QJ.14.-.-_071:128.00682,(C.-.-.QJ.04.-.-_071:125.53527,((C.-.-.QJ.11.-.-_198:237.71934,((C.-.-.QJ.19R.-.-_008:30.87580,C.-.-.QJ.05.-.-_071:93.87580):9.09507,C.-.-.QJ.15.-.-_071:102.97087):7.74847):1.35688,(C.-.-.QJ.02.-.-_344:377.75836,((((C.-.-.QJ.01.-.-_198:130.88335,(C.-.-.QJ.06.-.-_071:0.76698,C.-.-.QJ.01.-.-_071:0.76698):3.11637):40.76912,C.-.-.QJ.05.-.-_198:171.65247):27.28449,(C.-.-.QJ.04.-.-_198:96.93034,C.-.-.QJ.12.-.-_198:96.93034):102.00663):21.81617,(C.-.-.QJ.09.-.-_344:343.53552,(((C.-.-.QJ.04R.-.-_471:214.41347,((C.-.-.QJ.11R.-.-_471:72.25259,((C.-.-.QJ.02R.-.-_471:5.29524,C.-.-.QJ.17R.-.-_471:5.29524):35.56875,C.-.-.QJ.06R.-.-_471:40.86399):31.38860):26.89292,C.-.-.QJ.05R.-.-_471:99.14551):115.26796):100.64535,(C.-.-.QJ.09R.-.-_471:172.07219,((C.-.-.QJ.03.-.-_344:17.00900,(C.-.-.QJ.04R.-.-_377:31.51312,C.-.-.QJ.03R.-.-_471:125.51312):18.49588):9.66072,C.-.-.QJ.01.-.-_344:26.66971):18.40248):142.98663):93.05436,(C.-.-.QJ.15R.-.-_471:367.37135,((C.-.-.QJ.02R.-.-_377:85.63056,((C.-.-.QJ.03R.-.-_377:22.91475,C.-.-.QJ.06R.-.-_377:22.91475):26.07031,((C.-.-.QJ.09R.-.-_377:15.43959,C.-.-.QJ.07R.-.-_377:15.43959):21.98943,C.-.-.QJ.10R.-.-_377:37.42902):11.55603):36.64551):65.22438,C.-.-.QJ.08.-.-_344:117.85494):122.51640):40.74184):62.42234):23.21761):11.00522):7.31786):13.45905):2.47155):12.61969,C.-.-.QJ.17.-.-_071:140.62652):87.12035):53.44163):3.30199,((C.-.-.QJ.09.-.-_071:78.32393,(C.-.-.QJ.08.-.-_071:39.11806,(C.-.-.QJ.19.-.-_071:36.24508,(C.-.-.QJ.20.-.-_071:24.03750,C.-.-.QJ.10.-.-_071:24.03750):12.20758):2.87298):39.20587):69.01089,C.-.-.QJ.13.-.-_071:147.33482):137.15567):6.34994,C.-.-.QJ.02.-.-_071:290.84042):2.70237):1.40759):99.44262):228.64957,C.-.-.QJ.16.-.-_071:623.04257):94.07056,C.-.-.QJ.12.-.-_071:717.11313):0.00000;" )
 
 def getVRegions(vSeqFile):
 
@@ -88,7 +87,7 @@ def findChildren(node):
         i += 1
     return(None)
 
-def extractIndels(tip, anc, accno, vregion):
+def extractIndels(tip, anc, vregion):
     iTemp = ''
     dTemp = ''
     #print(accno)
@@ -183,8 +182,6 @@ def extractIndels(tip, anc, accno, vregion):
             if a != "-" or b != "-":
                 newvar[n] += a
                 newanc[n] += b
-    vSeq[accno] = newvar 
-    aSeq[accno] = newanc
     #print(vLen)
         
     #SANITY CHECK 
@@ -207,7 +204,7 @@ def extractIndels(tip, anc, accno, vregion):
             #print(extract_anc)
             #print(csvSeq)'''
 
-    return ({accno: insertions},{accno:deletions})
+    return insertions, deletions, newvar, newanc
     
 def addList(total, toAdd):
     for i in len(total):
@@ -226,6 +223,10 @@ def treeIndelExtract(node, vregion, data):
     insertions = {}
     deletions = {}
 
+    vseqs = {}
+    aseqs = {}
+
+
     child1, child2 = findChildren(node)
     #print(child1)
     #print("---")
@@ -235,34 +236,47 @@ def treeIndelExtract(node, vregion, data):
     #print(data[node])
 
     if isLeaf(child1):
-        output = extractIndels(data[child1], data[node], child1, vregion)
-        insertions.update(output[0])
-        deletions.update(output[1])
+        output = extractIndels(data[child1], data[node], vregion)
+        insertions.update({child1:output[0]})
+        deletions.update({child1:output[1]})
+        vseqs.update({child1:output[2]})
+        aseqs.update({child1:output[3]})
     else:       
         below = treeIndelExtract(child1, vregion, data)
-        insertions.update(below[0])
-        deletions.update(below[1])
+        insertions.update({child1:below[0]})
+        deletions.update({child1:below[1]})
+        vseqs.update({child1:below[2]})
+        aseqs.update({child1:below[3]})
 
-        output = extractIndels(data[child1], data[node], child1, vregion)
-        insertions.update(output[0])
-        deletions.update(output[1])
+        # 
+        output = extractIndels(data[child1], data[node], vregion)
+        insertions.update({child1:output[0]})
+        deletions.update({child1:output[1]})
+        vseqs.update({child1:output[2]})
+        aseqs.update({child1:output[3]})
 
     if isLeaf(child2):
-        output = extractIndels(data[child2], data[node], child2, vregion)
-        insertions.update(output[0])
-        deletions.update(output[1])
+        output = extractIndels(data[child2], data[node],vregion)
+        insertions.update({child2:output[0]})
+        deletions.update({child2:output[1]})
+        vseqs.update({child2:output[2]})
+        aseqs.update({child2:output[3]})
     else:
         below = treeIndelExtract(child2, vregion, data)
-        insertions.update(below[0])
-        deletions.update(below[1])
+        insertions.update({child2:below[0]})
+        deletions.update({child2:below[1]})
+        vseqs.update({child2:below[2]})
+        aseqs.update({child2:below[3]})
 
-        output = extractIndels(data[child2], data[node], child2, vregion)
-        insertions.update(output[0])
-        deletions.update(output[1])
+        output = extractIndels(data[child2], data[node], vregion)
+        insertions.update({child2:output[0]})
+        deletions.update({child2:output[1]})
+        vseqs.update({child2:output[2]})
+        aseqs.update({child2:output[3]})
     
     #rootData = extractIndels(data[node], "", node, vregion)
     #print(rootData)
-    return (insertions, deletions)
+    return (insertions, deletions, vseqs, aseqs)
     
     #------------------
     
@@ -289,84 +303,77 @@ for f in folder:
     infile = open(f, "rU")
     filename = os.path.basename(f)
     print(filename)
-    cdata = convert_fasta(infile)
 
-    trefile = filename.split("_recon")[0] + ".tree"
-    tree = Tree(tpath+trefile)
-    csvfile = filename.split('-')[0] + ".csv"
-    
-    root, rootseq = cdata[len(cdata)-1]
-    #print(root)
+    # retrieved the converted fasta 
+    cdata = convert_fasta(infile)
     infile.close()
 
-    #  LOADING VREGIONS 
-    # ------------------
+    # read the tree using the Tree using ETE3
+    trefile = filename.split("_recon")[0] + ".tree"
+    tree = Tree(tpath+trefile)
+    
+    # get the root label and sequence from the last entry of cdata
+    root, rootseq = cdata[len(cdata)-1]
+    
+
+    # get the boundaries from the 'variable regions' csvfile
     # returns a list of 1) vregion sequences and 2) boundaries for those sequences
+    csvfile = filename.split('-')[0] + ".csv"
     vregions, boundaries = getVRegions(vpath+csvfile)
         
-    # RETRIEVAL OF ALIGNMENT (vregion and relative position lists) using a SINGLE iteration of the first sequence
-    vregion = []
-    pos = []
     # previous = []  # sanity check to ensure that all vregion and pos lists are identical 
-
-
+    
     # CALIBRATION -- use an arbitrary sequence to calibrate the vregion list and the pos list
+    vidx = []
+    pos = []
     header, seq = cdata[0]
     ai = 0
     for n, char in enumerate(seq):
         #retrieves a numeric value (0,1,2,3,4) to indicate which variable region the nucleotide is in, and -1 if outside of a vloop
         #pos counts your position WITHIN the current variable loop 
         vr, p = vrSwitch(ai, boundaries[header])
-        vregion.append(vr)
+        vidx.append(vr)
         pos.append(p)
         if char != '-':
             ai += 1
-    # previous = vregion
-    #print(root)
+    # previous = vidx
+
     
     infile = open(f, "rU")
     pdata = parse_fasta(infile)
 
-    result = treeIndelExtract(root, vregion, pdata)
-    #print(result[0])
-    
+    result = treeIndelExtract(root, vidx, pdata)
 
-    #vseqs = ['','','','','']
-    #for n, char in enumerate(rootseq):
-    #    if vregion[n] != -1:
-    #        vseqs[vregion[n]] += char
-    #print({root:vseqs})
-    #result[0].update({root:vseqs})
-
-    tsvout = filename.split("_recon")[0] + ".tsv"   #.tsv
-    ioutput = open(opath+'ins/'+tsvout, 'w+')
-    doutput = open(opath+'del/'+tsvout,'w+') 
-    header = "header\tV1\tV2\tV3\tV4\tV5\n"
+    csvout = filename.split("_recon")[0] + ".csv"   #.tsv
+    ioutput = open(opath+'ins/'+csvout, 'w+')
+    doutput = open(opath+'del/'+csvout,'w+') 
+    header = "header,indel,vloop,vlen,tip,anc\n"
     ioutput.write(header)
     doutput.write(header)
-    #fastaout = open(opath+tsvout,"w+")
+    #fastaout = open(opath+csvout,"w+")
     #sys.exit()
-    for n, node in enumerate(result[0]):
-        #print(node)
-        #print(result[0][node])
-        data = [":".join(vloop) for vloop in result[0][node]] 
-        #data = "".join(result[0][node])   # for abayomi's output
-        #print(data)
-        #fastaout.write(">"+node+"\n"+data+"\n")
-        #print(",".join([node, data, "\n"]))
-        ioutput.write(node+"\t"+"\t".join(data)+"\n")
+    for accno in result[0]:
+        i = result[0][accno]
+        d = result[1][accno]
+        v = result[2][accno]
+        a = result[3][accno]
 
-    for n, node in enumerate(result[1]):
-        data = [":".join(vloop) for vloop in result[1][node]]
-        # print(data)
-        #print(",".join([node, data, "\n"]))
-        doutput.write(node+"\t"+"\t".join(data)+"\n")
+        for x, ins in enumerate(i):
+            inslist = ":".join(ins)
+            if inslist == "":
+                inslist = ""
+            newaccno = accno + "_" + str(x+1)
+            ioutput.write(",".join([newaccno, inslist, str(x+1), str(len(v[x])), v[x], a[x]]) + "\n")
+
+            dellist = ":".join(d[x])
+            if dellist == "":
+                dellist = ""
+            newaccno = accno + "_" + str(x+1)
+            doutput.write(",".join([newaccno, dellist, str(x+1), str(len(v[x])), v[x], a[x]]) + "\n")
+
     ioutput.close()
     doutput.close()
     
-    #tips = [ x for x in data.keys() if re.search('^[^\(\):\n]+$',x) != None]
-
-        
 
 
 
