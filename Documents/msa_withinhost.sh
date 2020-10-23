d278621a81098ded132c57fc17d37752c5c6b353
jpalme56 <jpalme56@uwo.ca>
2020-10-23 14:26:57 -0400

    removed documents folder

--- Documents/msa_withinhost.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-dir=/home/jpalmer/PycharmProjects/hiv-withinhost/4MSA/
-for filename in /home/jpalmer/PycharmProjects/hiv-withinhost/3RegionSequences/full_length/*.fasta; do
-	echo $filename
-	file="$(cut -d'/' -f8 <<<"$filename")"
-	echo $dir$file
-	mafft --auto "$filename" > "$dir$file"
-done
-
