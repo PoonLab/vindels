4e5174815a6806ce9b4ecb0cb0f3c432aab9b017
John Palmer <jpalme56@uwo.ca>
2021-03-23 10:43:41 -0400

    updated historian scripts

--- 2_within-host/historian.py
+++ 2_within-host/historian.py
@@ -6,16 +6,19 @@ from subprocess import check_call, PIPE, Popen, STDOUT
 import numpy as np
 import time
 
-if not sys.argv[1].endswith('/'):
-    sys.argv[1] += '/'
+if len(sys.argv) != 3:
+    print("USAGE python historian.py [tree folder] [output folder]")
+    sys.exit()
 
-tpath = sys.argv[1]+'trees/'
+for i in range(len(sys.argv)):
+    if not sys.argv[i].endswith('/'):
+        sys.argv[i] += '/'
 
+
+tpath = sys.argv[1]
 trees = [os.path.basename(x) for x in glob(tpath+"*")]
 
-
-tpath = sys.argv[1]+'trees/'
-opath = sys.argv[1]+'output/'
+opath = sys.argv[2]
 mpath = '/home/jpalmer/work/4MSA/final/'
 
 finished = [os.path.basename(x) for x in glob(opath+"*")]
@@ -27,6 +30,7 @@ out =  [opath+x.split(".tree")[0] + "_recon.fasta" for x in trees]
 trees = [tpath+x for x in trees]
 start = time.time()
 
+#print(len(trees))
 
 for i in range(len(trees)):
     print("Beginning analysis on file: " + trees[i]+"\n")
@@ -36,3 +40,4 @@ for i in range(len(trees)):
     outfile.close()
     current = time.time()
     print('Elapsed: {}'.format(current-start))
+
